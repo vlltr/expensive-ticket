494f21b7fbac2ffb66a8177e5bcaf388916ad57e
Mauricio Villatoro <mauripr26@gmail.com>
2023-03-10 02:46:33 +0000

    add vue and first component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-import './bootstrap';
+import "./bootstrap";
+
+import { createApp } from "vue/dist/vue.esm-bundler.js";
+import PostsIndex from "./components/Posts/Index.vue";
+
+const app = createApp({})
+
+app.component('posts-index', PostsIndex)
+app.mount('#app')
