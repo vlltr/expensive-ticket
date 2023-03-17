233b6dff2c8d83030f348429be8ccdddce6854e3
Mauricio Villatoro <mauripr26@gmail.com>
2023-03-17 03:38:35 +0000

    show title by route

--- resources/js/routes/index.js
+++ resources/js/routes/index.js
@@ -1,15 +1,20 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-
 import PostsIndex from "../components/Posts/Index.vue";
 import PostCreate from "../components/Posts/Create.vue";
 
 const routes = [
-    { path: "/", name:'posts.index', component: PostsIndex },
+    {
+        path: "/",
+        name: "posts.index",
+        component: PostsIndex,
+        meta: { title: "Posts" },
+    },
     {
         path: "/posts/create",
-        name:'posts.create',
+        name: "posts.create",
         component: PostCreate,
+        meta: { title: "Create Post" },
     },
 ];
 
