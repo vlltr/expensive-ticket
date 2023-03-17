348fe4db089264f95aaefb34452f067335ed6546
Mauricio Villatoro <mauripr26@gmail.com>
2023-03-17 02:56:23 +0000

    add view router and create first route

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,18 @@
 import "./bootstrap";
 
 import { createApp } from "vue/dist/vue.esm-bundler.js";
-import { TailwindPagination } from 'laravel-vue-pagination';
+import { createRouter, createWebHistory } from "vue-router";
+import { TailwindPagination } from "laravel-vue-pagination";
 import PostsIndex from "./components/Posts/Index.vue";
 
-const app = createApp({})
+const app = createApp({});
 
-app.component('posts-index', PostsIndex)
-app.component('Pagination', TailwindPagination)
-app.mount('#app')
+const routes = [{ path: "/", component: PostsIndex }];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+app.use(router)
+app.component("Pagination", TailwindPagination);
+app.mount("#app");
